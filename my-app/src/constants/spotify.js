950e0fe55e18ecdb9b6b335ddfbe4f41db0da79c
Melani330 <melanihasanah330@gmail.com>
2022-04-22 22:09:18 +0700

    Update spotify.js

--- my-app/src/constants/spotify.js
+++ my-app/src/constants/spotify.js
@@ -4,7 +4,7 @@ const RESPONSE_TYPE = "token";
 const CLIENT_ID = process.env.REACT_APP_SPOTIFY_API_KEY;
 const SCOPE = "playlist-modify-private playlist-modify-public user-read-private playlist-read-private playlist-read-collaborative";
 const REDIRECT_URI = process.env.NODE_ENV === 'production'
-  ? 'https://prmodul2-melanish.vercel.app'
+  ? 'https://final-project-melanish.vercel.app'
   : 'http://localhost:3000';
 
 export {
