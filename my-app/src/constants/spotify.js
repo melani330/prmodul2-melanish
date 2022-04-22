0b255b8c1dad1be8e967d9bba93dc7c58e186d0e
Melani330 <melanihasanah330@gmail.com>
2022-04-22 20:44:34 +0700

    modify spotify scope

--- my-app/src/constants/spotify.js
+++ my-app/src/constants/spotify.js
@@ -2,7 +2,7 @@ const AUTH_BASE_URL = "https://accounts.spotify.com/authorize";
 const API_BASE_URL = "https://api.spotify.com/v1";
 const RESPONSE_TYPE = "token";
 const CLIENT_ID = process.env.REACT_APP_SPOTIFY_API_KEY;
-const SCOPE = "playlist-modify-private";
+const SCOPE = "playlist-modify-private playlist-modify-public user-read-private playlist-read-private playlist-read-collaborative";
 const REDIRECT_URI = process.env.NODE_ENV === 'production'
   ? 'https://prmodul2-melanish.vercel.app'
   : 'http://localhost:3000';
